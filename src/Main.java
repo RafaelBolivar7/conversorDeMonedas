01d6ea976f5a4fa47764b001e26e6d9aba9d4df4
Rafael B <rafbolivar24@gmail.com>
2024-11-20 22:24:59 -0500

    Ciclo de repetición menu + switch case de eleccion

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
-import com.google.gson.Gson;
-
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -11,12 +12,11 @@ public class Main {
         //Instanciar la solicitud de Conversion
         SolicitudDeCambio solicitud = new SolicitudDeCambio();
         Calculo calculo = new Calculo(solicitud);
+        //GeneradorArchivos generador = new GeneradorArchivos();
 
+        List<String> respuestas = new ArrayList<>();
 
-
-
-        while (true){
-            System.out.println("""
+        String menu = """
                     ******************************************
                     ¡Bienvenido/a al conversor de monedas!
                     1) Dólar =>> Peso Argentino
@@ -27,14 +27,46 @@ public class Main {
                     6) Peso Colombiano =>> Dolar
                     7) Salir
                     
-                    Elija una opción valida: 
+                    Elija una opción valida:
                     
                     ******************************************
-                    """);
+                    """;
+
+        while (opcion != 7){
+            try{
+                System.out.println(menu);
+                opcion = Integer.parseInt(lectura.nextLine());
+
+                switch (opcion){
+                    case 1:
+                        calculo.almacenaValores("USD", "ARS");
+                        respuestas.add(calculo.getMensajeRespuesta());
+                    case 2:
+                        calculo.almacenaValores("ARS", "USD");
+                        respuestas.add(calculo.getMensajeRespuesta());
+                    case 3:
+                        calculo.almacenaValores("USD", "BRL");
+                        respuestas.add(calculo.getMensajeRespuesta());
+                    case 4:
+                        calculo.almacenaValores("BRL", "USD");
+                        respuestas.add(calculo.getMensajeRespuesta());
+                    case 5:
+                        calculo.almacenaValores("USD", "COP");
+                        respuestas.add(calculo.getMensajeRespuesta());
+                    case 6:
+                        calculo.almacenaValores("COP", "USD");
+                        respuestas.add(calculo.getMensajeRespuesta());
+                    case 7:
+                        break;
+                    default:
+                        System.out.println("Ingrese una opción válida");
+                }
+
+            }  catch (NumberFormatException |InputMismatchException e) {
+                System.out.println("Error. Ingrese un valor numérico válido.");
+            }
         }
-
-
-
-
+        //generador.guardarJson(respuestas);
+        System.out.println("¡Programa Finalizado!");
     }
 }
